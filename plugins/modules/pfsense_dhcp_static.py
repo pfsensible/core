a6b5bed4e26d726ad5752708a32b3b64e8f3a5c9
Orion Poplawski <orion@nwra.com>
2023-01-23 21:28:02 -0700

    [pfsense_dhcp_static] Various fixes
    - Fix adding initial static entry
    - Add the default XML elements
    - Define ddnsdomainkeyalgorithm choices
    - Validate MAC addresses
    
    Tests:
    - That ipaddr is in the subnet of the interface
    - That there only be one enabled DHCP interface if netif is not
      specified

--- plugins/modules/pfsense_dhcp_static.py
+++ plugins/modules/pfsense_dhcp_static.py
@@ -22,14 +22,16 @@ description:
 notes:
 options:
   name:
-    description: The client name identifier
-    required: true
+    description: The client name identifier.  At least one of I(name) or I(macaddr) is required.
     type: str
+    aliases:
+      - cid
   netif:
-    description: The network interface
+    description: >
+      The network interface.  This defaults to the only enabled DHCP interface if there is only one.
     type: str
   macaddr:
-    description: The mac address
+    description: The mac address.  At least one of I(name) or I(macaddr) is required.
     type: str
   ipaddr:
     description: The IP address
@@ -88,6 +90,7 @@ options:
   ddnsdomainkeyalgorithm:
     description: The ddns key algorithm
     type: str
+    choices: [ 'hmac-md5', 'hmac-sha1', 'hmac-sha224', 'hmac-sha256', 'hmac-sha384', 'hmac-sha512' ]
   ddnsdomainkey:
     description: The ddns domain key
     type: str
@@ -111,6 +114,9 @@ options:
     type: str
   filename64arm:
     description: The filename for 64arm
+    type: str
+  uefihttpboot:
+    description: UEFI HTTPBoot URL
     type: str
   numberoptions:
     description: The number options
@@ -140,13 +146,16 @@ RETURN = """
 
 """
 
+from ipaddress import ip_address, ip_network
+import re
+
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.pfsensible.core.plugins.module_utils.module_base import PFSenseModuleBase
 
 DHCP_STATIC_ARGUMENT_SPEC = dict(
-    name=dict(required=True, type='str'),
-    netif=dict(type='str'),
+    name=dict(type='str', aliases=['cid']),
     macaddr=dict(type='str'),
+    netif=dict(type='str'),
     ipaddr=dict(type='str'),
     hostname=dict(type='str'),
     descr=dict(type='str'),
@@ -164,7 +173,7 @@ DHCP_STATIC_ARGUMENT_SPEC = dict(
     ddnsdomainprimary=dict(type='str'),
     ddnsdomainsecondary=dict(type='str'),
     ddnsdomainkeyname=dict(type='str'),
-    ddnsdomainkeyalgorithm=dict(type='str'),
+    ddnsdomainkeyalgorithm=dict(type='str', choices=['hmac-md5', 'hmac-sha1', 'hmac-sha224', 'hmac-sha256', 'hmac-sha384', 'hmac-sha512']),
     ddnsdomainkey=dict(type='str', no_log=True),
     tftp=dict(type='str'),
     ldap=dict(type='str'),
@@ -173,16 +182,17 @@ DHCP_STATIC_ARGUMENT_SPEC = dict(
     filename64=dict(type='str'),
     filename32arm=dict(type='str'),
     filename64arm=dict(type='str'),
+    uefihttpboot=dict(type='str'),
     numberoptions=dict(type='str'),
     state=dict(type='str', default='present', choices=['present', 'absent']),
 )
 
-DHCP_STATIC_REQUIRED_IF = [
-    ["state", "present", ["name", "macaddr", "ipaddr"]],
+DHCP_STATIC_REQUIRED_ONE_OF = [
+    ('name', 'macaddr'),
 ]
 
 
-class PFSenseDHCPSTATICModule(PFSenseModuleBase):
+class PFSenseDHCPStaticModule(PFSenseModuleBase):
     """ module managing pfsense dhcp static configuration """
 
     @staticmethod
@@ -194,7 +204,7 @@ class PFSenseDHCPSTATICModule(PFSenseModuleBase):
     # init
     #
     def __init__(self, module, pfsense=None):
-        super(PFSenseDHCPSTATICModule, self).__init__(module, pfsense)
+        super(PFSenseDHCPStaticModule, self).__init__(module, pfsense)
         self.name = "pfsense_dhcp_static"
         self.dhcpd = self.pfsense.get_element('dhcpd')
         self.root_elt = None
@@ -208,8 +218,19 @@ class PFSenseDHCPSTATICModule(PFSenseModuleBase):
 
         params = self.params
 
-        if params['state'] == 'absent':
-            return
+        if re.fullmatch(r'(?:[0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}', params['macaddr']) is None:
+            self.module.fail_json(msg='A valid MAC address must be specified.')
+
+        if params['netif'] is not None:
+            self.pfsense.parse_interface(params['netif'])
+
+        # find staticmaps and determine interface
+        self._find_staticmaps(params['netif'])
+
+        if params['ipaddr'] is not None:
+            addr = ip_address(u'{0}'.format(params['ipaddr']))
+            if addr not in self.network:
+                self.module.fail_json(msg='The IP address must lie in the {0} subnet.'.format(self.netif))
 
     def _params_to_obj(self):
         """ return a dict from module params """
@@ -218,27 +239,25 @@ class PFSenseDHCPSTATICModule(PFSenseModuleBase):
         obj = dict()
         self.obj = obj
         # client identifier
-        obj['cid'] = params['name']
-
-        # find staticmaps
-        self._find_staticmaps(params['netif'])
+        self._get_ansible_param(obj, 'name', fname='cid', force=True)
 
         if params['state'] == 'present':
 
-            obj['mac'] = params['macaddr']
-            obj['ipaddr'] = params['ipaddr']
-
-            # other options
-            for option in ['hostname', 'descr', 'filename',
+            self._get_ansible_param(obj, 'macaddr', fname='mac', force=True)
+            # Forced options
+            for option in ['ipaddr', 'hostname', 'descr', 'filename',
                            'rootpath', 'defaultleasetime', 'maxleasetime',
                            'gateway', 'domain', 'domainsearchlist',
-                           'winsserver', 'dnsserver', 'ntpserver',
                            'ddnsdomain', 'ddnsdomainprimary', 'ddnsdomainsecondary',
                            'ddnsdomainkeyname', 'ddnsdomainkeyalgorithm', 'ddnsdomainkey',
                            'tftp', 'ldap', 'nextserver', 'filename32', 'filename64',
-                           'filename32arm', 'filename64arm', 'numberoptions']:
-                if option in params and params[option] is not None:
-                    obj[option] = params[option]
+                           'filename32arm', 'filename64arm', 'uefihttpboot', 'numberoptions']:
+                self._get_ansible_param(obj, option, force=True)
+            # Non-forced options
+            for option in ['winsserver', 'dnsserver', 'ntpserver']:
+                self._get_ansible_param(obj, option)
+            # Defaulted options
+            self._get_ansible_param(obj, 'ddnsdomainkeyalgorithm', force_value='hmac-md5', force=True)
 
         return obj
 
@@ -254,6 +273,7 @@ class PFSenseDHCPSTATICModule(PFSenseModuleBase):
                         if nic.find('subnet') is not None:
                             subnet = int(nic.find('subnet').text)
                             if subnet < 31:
+                                self.network = ip_network(u'{0}/{1}'.format(ipaddr, subnet), strict=False)
                                 return True
         return False
 
@@ -262,26 +282,34 @@ class PFSenseDHCPSTATICModule(PFSenseModuleBase):
             if netif is None or e.tag == netif:
                 if e.find('enable') is not None:
                     if self._is_valid_netif(e.tag):
+                        if self.root_elt is not None:
+                            self.module.fail_json(msg='Multiple DHCP servers enabled and no netif specified')
                         self.root_elt = e
+                        self.netif = e.tag
                         self.staticmaps = self.root_elt.findall('staticmap')
-                        break
+                        if netif is not None:
+                            break
+
         if self.root_elt is None:
-            self.module.fail_json(msg='No DHCP configuration')
+            if netif is None:
+                self.module.fail_json(msg="No DHCP configuration")
+            else:
+                self.module.fail_json(msg="No DHCP configuration found for netif='{0}'".format(netif))
 
     def _find_target(self):
-        result = self.root_elt.findall("staticmap[cid='{0}']".format(self.obj['cid']))
+        if self.params['name'] is not None and self.params['macaddr'] is not None:
+            result = self.root_elt.findall("staticmap[cid='{0}'][mac='{1}']".format(self.params['name'], self.params['macaddr']))
+        elif self.params['name'] is not None:
+            result = self.root_elt.findall("staticmap[cid='{0}']".format(self.params['name']))
+        else:
+            result = self.root_elt.findall("staticmap[mac='{0}']".format(self.params['macaddr']))
+
         if len(result) == 1:
             return result[0]
         elif len(result) > 1:
             self.module.fail_json(msg='Found multiple static maps for cid {0}.'.format(self.obj['cid']))
         else:
             return None
-
-    def _find_this_dhcp_static_index(self):
-        return self.staticmaps.index(self.target_elt)
-
-    def _find_last_dhcp_static_index(self):
-        return list(self.root_elt).index(self.staticmaps[len(self.staticmaps) - 1])
 
     def _create_target(self):
         """ create the XML target_elt """
@@ -293,7 +321,7 @@ class PFSenseDHCPSTATICModule(PFSenseModuleBase):
 
         self.diff['after'] = obj
         self.pfsense.copy_dict_to_element(self.obj, self.target_elt)
-        self.root_elt.insert(self._find_last_dhcp_static_index(), self.target_elt)
+        self.root_elt.append(self.target_elt)
         # Reset static map list
         self.staticmaps = self.root_elt.findall('staticmap')
 
@@ -319,9 +347,11 @@ class PFSenseDHCPSTATICModule(PFSenseModuleBase):
         """ generate pseudo-CLI command fields parameters to create an obj """
         values = ''
         if before is None:
-            values += self.format_cli_field(self.params, 'name')
+            values += self.format_cli_field(self.params, 'macaddr')
+            values += self.format_cli_field(self.params, 'ipaddr')
         else:
-            values += self.format_updated_cli_field(self.obj, before, 'cid', add_comma=(values))
+            values += self.format_updated_cli_field(self.obj, before, 'macaddr', add_comma=(values))
+            values += self.format_updated_cli_field(self.obj, before, 'ipaddr', add_comma=(values))
         return values
 
     ##############################
@@ -350,10 +380,10 @@ class PFSenseDHCPSTATICModule(PFSenseModuleBase):
 def main():
     module = AnsibleModule(
         argument_spec=DHCP_STATIC_ARGUMENT_SPEC,
-        required_if=DHCP_STATIC_REQUIRED_IF,
+        required_one_of=DHCP_STATIC_REQUIRED_ONE_OF,
         supports_check_mode=True)
 
-    pfmodule = PFSenseDHCPSTATICModule(module)
+    pfmodule = PFSenseDHCPStaticModule(module)
     pfmodule.run(module.params)
     pfmodule.commit_changes()
 
