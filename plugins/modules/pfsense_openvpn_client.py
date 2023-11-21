131cdd79e8747f09d06b591f50578133308eb7d8
SherZCHR <65924866+SherZCHR@users.noreply.github.com>
2023-11-21 11:50:32 +0100

    Update pfsense_openvpn_client.py

--- plugins/modules/pfsense_openvpn_client.py
+++ plugins/modules/pfsense_openvpn_client.py
@@ -82,7 +82,7 @@ options:
     required: false
     default: 1
     type: int
-  strictuserdn:
+  strictusercn:
     description: Enforce a match between the common name of the client certificate and the username given at login.
     default: false
     type: bool
