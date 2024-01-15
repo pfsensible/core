f1da18e946ea4ee4571eb9588953000a06f72919
Orion Poplawski <orion@nwra.com>
2024-01-14 22:12:20 -0700

    [pfsense_authserver_ldap] Update copyright

--- plugins/modules/pfsense_authserver_ldap.py
+++ plugins/modules/pfsense_authserver_ldap.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2018-2020, Orion Poplawski <orion@nwra.com>
+# Copyright: (c) 2018-2024, Orion Poplawski <orion@nwra.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
