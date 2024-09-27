f3a517330db2e746bf4838faf3a2cae23e0127a4
David Rosado <drosza@proton.me>
2024-09-27 13:36:04 -0400

    dhcp server initial and tests

---
+++ tests/unit/plugins/modules/test_pfsense_dhcp_server.py
@@ -0,0 +1,118 @@
+import pytest
+import sys
+
+if sys.version_info < (2, 7):
+    pytestmark = pytest.mark.skip("pfSense Ansible modules require Python >= 2.7")
+
+from ansible_collections.pfsensible.core.plugins.modules import pfsense_dhcp_server
+from ansible_collections.pfsensible.core.plugins.modules.pfsense_dhcp_server import PFSenseDHCPDServerModule
+from .pfsense_module import TestPFSenseModule
+
+class TestPFSenseDHCPServerModule(TestPFSenseModule):
+
+    module = pfsense_dhcp_server
+
+    def __init__(self, *args, **kwargs):
+        super(TestPFSenseDHCPServerModule, self).__init__(*args, **kwargs)
+        self.config_file = 'pfsense_dhcp_server_config.xml'
+        self.pfmodule = PFSenseDHCPDServerModule
+
+    def check_target_elt(self, obj, target_elt, target_idx=-1):
+        """ test the xml definition """
+        self.check_param_equal(obj, target_elt, 'interface')
+        self.check_param_equal(obj, target_elt, 'enable')
+        self.check_param_equal_or_present(obj, target_elt, 'range_from', xml_field='range/from')
+        self.check_param_equal_or_present(obj, target_elt, 'range_to', xml_field='range/to')
+        self.check_param_equal(obj, target_elt, 'failover_peerip')
+        self.check_param_equal(obj, target_elt, 'defaultleasetime')
+        self.check_param_equal(obj, target_elt, 'maxleasetime')
+        self.check_param_equal(obj, target_elt, 'netmask')
+        self.check_param_equal(obj, target_elt, 'gateway')
+        self.check_param_equal(obj, target_elt, 'domain')
+        self.check_param_equal(obj, target_elt, 'domainsearchlist')
+        self.check_param_equal(obj, target_elt, 'ddnsdomain')
+        self.check_param_equal(obj, target_elt, 'ddnsdomainprimary')
+        self.check_param_equal(obj, target_elt, 'ddnsdomainkeyname')
+        self.check_param_equal(obj, target_elt, 'ddnsdomainkeyalgorithm')
+        self.check_param_equal(obj, target_elt, 'ddnsdomainkey')
+        self.check_param_equal(obj, target_elt, 'mac_allow')
+        self.check_param_equal(obj, target_elt, 'mac_deny')
+        self.check_param_equal(obj, target_elt, 'tftp')
+        self.check_param_equal(obj, target_elt, 'ldap')
+        self.check_param_equal(obj, target_elt, 'nextserver')
+        self.check_param_equal(obj, target_elt, 'filename')
+        self.check_param_equal(obj, target_elt, 'filename32')
+        self.check_param_equal(obj, target_elt, 'filename64')
+        self.check_param_equal(obj, target_elt, 'rootpath')
+        self.check_param_equal(obj, target_elt, 'numberoptions')
+
+    def get_target_elt(self, obj, absent=False, module_result=None):
+        """ get the generated xml definition """
+        root_elt = self.assert_find_xml_elt(self.xml_result, 'dhcpd')
+        return root_elt.find(obj['interface'])
+
+    ##############
+    # tests
+    #
+    def test_dhcp_server_create(self):
+        """ test creation of a new DHCP server """
+        obj = dict(
+            interface='opt2',
+            enable=True,
+            range_from='172.16.0.100',
+            range_to='172.16.0.199',
+            defaultleasetime=86400,
+            maxleasetime=172800,
+            domain='opt2.example.com'
+        )
+        command = "create dhcp_server 'opt2', enable=True, range_from='172.16.0.100', range_to='172.16.0.199', defaultleasetime='86400', maxleasetime='172800', domain='opt2.example.com'"
+        self.do_module_test(obj, command=command)
+
+    def test_dhcp_server_update(self):
+        """ test updating an existing DHCP server """
+        obj = dict(
+            interface='lan',
+            enable=True,
+            range_from='192.168.1.50',
+            range_to='192.168.1.150',
+            domain='updated.example.com'
+        )
+        command = "update dhcp_server 'lan' set enable=True, range_from='192.168.1.50', range_to='192.168.1.150', domain='updated.example.com'"
+        self.do_module_test(obj, command=command)
+
+    def test_dhcp_server_delete(self):
+        """ test deletion of a DHCP server """
+        obj = dict(interface='opt1', state='absent')
+        command = "delete dhcp_server 'opt1'"
+        self.do_module_test(obj, command=command, delete=True)
+
+    def test_dhcp_server_create_invalid_interface(self):
+        """ test creation with an invalid interface """
+        obj = dict(interface='invalid_interface', enable=True, range_from='192.168.1.100', range_to='192.168.1.200')
+        self.do_module_test(obj, failed=True, msg='The interface invalid_interface does not exist')
+
+    def test_dhcp_server_create_invalid_range(self):
+        """ test creation with an invalid IP range """
+        obj = dict(interface='lan', enable=True, range_from='192.168.1.200', range_to='192.168.1.100')
+        self.do_module_test(obj, failed=True, msg='The start of the IP range must be less than the end of the IP range')
+
+    def test_dhcp_server_create_with_options(self):
+        """ test creation with additional DHCP options """
+        obj = dict(
+            interface='opt1',
+            enable=True,
+            range_from='10.0.0.50',
+            range_to='10.0.0.150',
+            defaultleasetime=43200,
+            maxleasetime=86400,
+            domain='opt1.example.com',
+            ddnsdomain='ddns.example.com',
+            ddnsdomainprimary='10.0.0.2',
+            tftp='10.0.0.3',
+            numberoptions=[
+                dict(number=66, type='string', value='10.0.0.4'),
+                dict(number=67, type='string', value='pxeboot.0')
+            ]
+        )
+        command = "create dhcp_server 'opt1', enable=True, range_from='10.0.0.50', range_to='10.0.0.150', defaultleasetime='43200', maxleasetime='86400', domain='opt1.example.com', ddnsdomain='ddns.example.com', ddnsdomainprimary='10.0.0.2', tftp='10.0.0.3', numberoptions='[{\"number\": 66, \"type\": \"string\", \"value\": \"10.0.0.4\"}, {\"number\": 67, \"type\": \"string\", \"value\": \"pxeboot.0\"}]'"
+        self.do_module_test(obj, command=command)
